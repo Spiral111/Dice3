e19b43ffa2a8edd09e31874a7a139e766c9a75fe
Jigsaw <1290541225@qq.com>
2020-07-01 09:28:19 +0800

    增加dice_db.cpp和dice_bot_module.cpp的注释

--- src/dice_bot_module.cpp
+++ src/dice_bot_module.cpp
@@ -1,3 +1,5 @@
+//机器人控制
+
 #include "dice_bot_module.h"
 #include "SQLiteCpp/SQLiteCpp.h"
 #include "cqsdk/cqsdk.h"
@@ -6,62 +8,87 @@
 #include "dice_utils.h"
 #include "dice_msg_queue.h"
 
-namespace cq::event {
+namespace cq::event 
+{
     struct MessageEvent;
 }
 
-namespace dice {
-    bool bot_module::match(const cq::event::MessageEvent &e, const std::wstring &ws) {
+namespace dice 
+{
+    //匹配正则
+    bool bot_module::match(const cq::event::MessageEvent &e, const std::wstring &ws) 
+    {
         std::wregex re(L"[\\s]*[\\.。．][\\s]*bot.*", std::regex_constants::ECMAScript | std::regex_constants::icase);
         if (std::regex_match(ws, re)) return true;
         return search_db(e);
     }
 
-    bool bot_module::search_db(const cq::event::MessageEvent &e) {
-        if (e.message_type == cq::message::DISCUSS) {
+    bool bot_module::search_db(const cq::event::MessageEvent &e) 
+    {
+        //如果在讨论组里则查群信息表
+        if (e.message_type == cq::message::DISCUSS) 
+        {
             SQLite::Statement statement(*db::db,
                                         "SELECT bot_on, is_ban FROM group_info WHERE group_id = ? AND type = ?");
             statement.bind(1, *e.target.discuss_id);
             statement.bind(2, 1);
-            if (statement.executeStep()) {
+            if (statement.executeStep()) 
+            {
                 return !statement.getColumn(0).getInt() || statement.getColumn(1).getInt();
             }
             return false;
         }
-        if (e.message_type == cq::message::GROUP) {
+
+        //如果在群里则查群信息表
+        if (e.message_type == cq::message::GROUP) 
+        {
             SQLite::Statement statement(*db::db,
                                         "SELECT bot_on, is_ban FROM group_info WHERE group_id = ? AND type = ?");
             statement.bind(1, *e.target.group_id);
             statement.bind(2, 0);
-            if (statement.executeStep()) {
+            if (statement.executeStep()) 
+            {
                 return !statement.getColumn(0).getInt() || statement.getColumn(1).getInt();
             }
             return false;
         }
+        
+        //查用户信息表
         SQLite::Statement statement(*db::db, "SELECT bot_on, is_ban FROM qq_info WHERE qq_id = ?");
         statement.bind(1, *e.target.user_id);
-        if (statement.executeStep()) {
+        if (statement.executeStep()) 
+        {
             return !statement.getColumn(0).getInt() || statement.getColumn(1).getInt();
         }
         return false;
     }
 
-    void bot_module::update_db(const cq::event::MessageEvent &e, bool bot_on) {
-        if (e.message_type == cq::message::DISCUSS) {
+    //自动更新数据库
+    void bot_module::update_db(const cq::event::MessageEvent &e, bool bot_on) 
+    {
+        //讨论组
+        if (e.message_type == cq::message::DISCUSS) 
+        {
             SQLite::Statement statement(*db::db,
                                         "REPLACE INTO group_info(group_id, type, bot_on) VALUES(?, ?, ?)");
             statement.bind(1, *e.target.discuss_id);
             statement.bind(2, 1);
             statement.bind(3, bot_on);
             statement.exec();
-        } else if (e.message_type == cq::message::GROUP) {
+        } 
+        //群
+        else if (e.message_type == cq::message::GROUP) 
+        {
             SQLite::Statement statement(*db::db,
                                         "REPLACE INTO group_info(group_id, type, bot_on) VALUES(?, ?, ?)");
             statement.bind(1, *e.target.group_id);
             statement.bind(2, 0);
             statement.bind(3, bot_on);
             statement.exec();
-        } else {
+        } 
+        //私聊
+        else
+        {
             SQLite::Statement statement(*db::db,
                                         "REPLACE INTO qq_info(qq_id, bot_on) VALUES(?, ?)");
             statement.bind(1, *e.target.user_id);
@@ -70,31 +97,46 @@ namespace dice {
         }
     }
 
-    void bot_module::process(const cq::event::MessageEvent &e, const std::wstring &ws) {
+    //机器人开关
+    void bot_module::process(const cq::event::MessageEvent &e, const std::wstring &ws) 
+    {
         std::wregex re(L"[\\s]*[\\.。．][\\s]*bot[\\s]*(on|off)?[\\s]*(.*)",
                        std::regex_constants::ECMAScript | std::regex_constants::icase);
         std::wsmatch m;
-        if (std::regex_match(ws, m, re)) {
+        if (std::regex_match(ws, m, re)) 
+        {
             std::wstring command = m[1];
             std::wstring target = m[2];
             std::wstring self_id = std::to_wstring(cq::api::get_login_user_id());
+            //响应空/qq号/qq号后四位/qq昵称
             if (target.empty() || target == self_id || target == self_id.substr(self_id.length() - 4)
-                || target == cq::utils::s2ws(cq::api::get_login_nickname())) {
-                if (command == L"on") {
-                    if (e.message_type == cq::message::GROUP && !utils::is_admin_or_owner(e.target)) {
+                || target == cq::utils::s2ws(cq::api::get_login_nickname())) 
+            {
+                //开
+                if (command == L"on") 
+                {
+                    if (e.message_type == cq::message::GROUP && !utils::is_admin_or_owner(e.target)) 
+                    {
                         dice::msg_queue::MsgQueue.add(e.target, msg::GetGlobalMsg("strPermissionDeniedError"));
                         return;
                     }
                     update_db(e, true);
                     dice::msg_queue::MsgQueue.add(e.target, msg::GetGlobalMsg("strEnabled"));
-                } else if (command == L"off") {
-                    if (e.message_type == cq::message::GROUP && !utils::is_admin_or_owner(e.target)) {
+                } 
+                //关
+                else if (command == L"off") 
+                {
+                    if (e.message_type == cq::message::GROUP && !utils::is_admin_or_owner(e.target)) 
+                    {
                         dice::msg_queue::MsgQueue.add(e.target, msg::GetGlobalMsg("strPermissionDeniedError"));
                         return;
                     }
                     update_db(e, false);
                     dice::msg_queue::MsgQueue.add(e.target, msg::GetGlobalMsg("strDisabled"));
-                } else {
+                } 
+                //如果无开关指令则回复机器人信息
+                else 
+                {
                     dice::msg_queue::MsgQueue.add(e.target, msg::dice_full_info);
                 }
             }
--- src/dice_db.cpp
+++ src/dice_db.cpp
@@ -5,11 +5,15 @@
 #include "dice_exception.h"
 #include "dice_msg.h"
 
-namespace dice::db {
+namespace dice::db 
+{
     std::unique_ptr<SQLite::Database> db;
 
+    //初始化数据库，如果表不存在则创建新表
     void InitialiseDB() {
         SQLite::Transaction tran(*db);
+
+        //QQ信息
         db->exec(
             "CREATE TABLE IF NOT EXISTS qq_info (qq_id INTEGER PRIMARY KEY NOT NULL, is_ban INTEGER DEFAULT 0, "
             "ban_reason "
@@ -19,24 +23,36 @@ namespace dice::db {
             "INTEGER, jrrp_date "
             "TEXT, bot_on INTEGER DEFAULT 1, card_chosen TEXT DEFAULT \"default\", nick_name TEXT, default_dice "
             "INTEGER NOT NULL DEFAULT 100, success_rule INTEGER NOT NULL DEFAULT 0 CHECK (success_rule >= 0 AND success_rule <= 5))");
+        
+        //角色卡
         db->exec(
             "CREATE TABLE IF NOT EXISTS character_cards (qq_id INTEGER NOT NULL, card_name TEXT NOT NULL, property "
             "TEXT NOT NULL, value INTEGER NOT NULL, "
             "PRIMARY KEY (qq_id, card_name, property))");
+        
+        //群信息
         db->exec(
             "CREATE TABLE IF NOT EXISTS group_info (group_id INTEGER NOT NULL, type INTEGER NOT NULL, bot_on INTEGER "
             "DEFAULT 1, help_on INTEGER DEFAULT 1, jrrp_on INTEGER DEFAULT 1, is_ban INTEGER DEFAULT 0, ban_reason "
             "TEXT, ban_time INTEGER, "
             "is_white "
             "INTEGER DEFAULT 0, white_time INTEGER, default_dice INTEGER NOT NULL DEFAULT 100, success_rule INTEGER NOT NULL DEFAULT 0 CHECK (success_rule >= 0 AND success_rule <= 5), PRIMARY KEY(group_id, type))");
+        
+        //群员信息
         db->exec(
             "CREATE TABLE IF NOT EXISTS group_user_info (qq_id INTEGER NOT NULL, group_id INTEGER NOT NULL, type "
             "INTEGER "
             "NOT NULL, nick_name TEXT, card_chosen TEXT DEFAULT \"default\", PRIMARY KEY(qq_id, group_id, type))");
+        
+        //全局消息
         db->exec("CREATE TABLE IF NOT EXISTS global_msg (title TEXT PRIMARY KEY NOT NULL, val TEXT NOT NULL)");
+        
+        //帮助信息
         db->exec("CREATE TABLE IF NOT EXISTS help_msg (title TEXT PRIMARY KEY NOT NULL, val TEXT NOT NULL)");
-        if (db->execAndGet("SELECT count(*) FROM sqlite_master WHERE type = \"table\" AND name = \"deck\"").getInt() ==
-            0) {
+
+        //牌堆
+        if (db->execAndGet("SELECT count(*) FROM sqlite_master WHERE type = \"table\" AND name = \"deck\"").getInt() ==0) 
+            {
             db->exec(
                 "CREATE TABLE deck(name TEXT NOT NULL, content TEXT NOT NULL, origin TEXT NOT NULL "
                 "DEFAULT "
@@ -44,8 +60,10 @@ namespace dice::db {
             db->exec("CREATE INDEX idx_name ON deck(name)");
             db->exec("CREATE INDEX idx_ori ON deck(origin)");
 
-            for (const auto &msg : msg::default_deck) {
-                for (const auto &item : msg.second) {
+            for (const auto &msg : msg::default_deck) 
+            {
+                for (const auto &item : msg.second) 
+                {
                     SQLite::Statement st(*db, "INSERT INTO deck(name, content) VALUES(?, ?)");
                     st.bind(1, msg.first);
                     st.bind(2, item);
@@ -54,13 +72,18 @@ namespace dice::db {
             }
         }
 
-        for (const auto &msg : msg::global_msg) {
+        //全局信息
+        for (const auto &msg : msg::global_msg) 
+        {
             SQLite::Statement st(*db, "INSERT OR IGNORE INTO global_msg(title, val) VALUES(?, ?)");
             st.bind(1, msg.first);
             st.bind(2, msg.second);
             st.exec();
         }
-        for (const auto &msg : msg::help_msg) {
+
+        //帮助信息
+        for (const auto &msg : msg::help_msg) 
+        {
             SQLite::Statement st(*db, "INSERT OR IGNORE INTO help_msg(title, val) VALUES(?, ?)");
             st.bind(1, msg.first);
             st.bind(2, msg.second);
@@ -69,15 +92,19 @@ namespace dice::db {
         tran.commit();
     }
 
-    void SemiReplaceDB() {
+    //半重置数据库
+    void SemiReplaceDB() 
+    {
         SQLite::Transaction tran(*db);
-        for (const auto &msg : msg::global_msg) {
+        for (const auto &msg : msg::global_msg) 
+        {
             SQLite::Statement st(*db, "REPLACE INTO global_msg(title, val) VALUES(?, ?)");
             st.bind(1, msg.first);
             st.bind(2, msg.second);
             st.exec();
         }
-        for (const auto &msg : msg::help_msg) {
+        for (const auto &msg : msg::help_msg) 
+        {
             SQLite::Statement st(*db, "REPLACE INTO help_msg(title, val) VALUES(?, ?)");
             st.bind(1, msg.first);
             st.bind(2, msg.second);
@@ -91,8 +118,10 @@ namespace dice::db {
         db->exec("CREATE INDEX idx_name ON deck(name)");
         db->exec("CREATE INDEX idx_ori ON deck(origin)");
 
-        for (const auto &msg : msg::default_deck) {
-            for (const auto &item : msg.second) {
+        for (const auto &msg : msg::default_deck) 
+        {
+            for (const auto &item : msg.second) 
+            {
                 SQLite::Statement st(*db, "INSERT OR IGNORE INTO deck(name, content) VALUES(?, ?)");
                 st.bind(1, msg.first);
                 st.bind(2, item);
@@ -102,10 +131,13 @@ namespace dice::db {
         tran.commit();
     }
 
-    void ReplaceDB() {
+    //重置数据库
+    void ReplaceDB() 
+    {
         db = nullptr;
         if (!std::filesystem::remove(cq::utils::s2ws(cq::api::get_app_directory() + "DiceConfig_"
-                                                     + std::to_string(cq::api::get_login_user_id()) + ".db"))) {
+                                                     + std::to_string(cq::api::get_login_user_id()) + ".db"))) 
+        {
             cq::logging::debug("Dice! V3", "Unable to locate database");
         }
         db = std::make_unique<SQLite::Database>(
